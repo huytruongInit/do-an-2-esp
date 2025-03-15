eefaba15fe20d8188df4c359fde2d76e632bf9cd
huy truong <huytruong.project@gmail.com>
2025-03-15 10:03:40 +0700

    Buổi 2 + Sử dụng VSCODE + Khởi tạo các ngoại vi, cảm biến

---
+++ lib/MY_APPLICATION/secret.h
@@ -0,0 +1,7 @@
+#ifndef MY_SECRET
+#define MY_SECRET
+
+#define API_KEY "AIzaSyB_scuWAgYKmoBqG_33oBAWrLLSHp9sVq0"    // Insert Firebase project API Key
+#define DATABASE_URL "https://digit-door-lock-default-rtdb.firebaseio.com"  // Insert RTDB URLefine the RTDB URL */
+
+#endif
